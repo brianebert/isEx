ed92e72187b06a7b25b0cfd5f2c19cc61fd8e34c
Brian Ebert <brian@motia.com>
2019-06-01 14:51:11 -0500

    wip

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,41 +6,42 @@ const welcome = new Page([new Section([new Div(`
     <div>
       <img src="./me.jpg" style="float:left;width:240px;height:250px;border-radius:20%;margin-left:10%;margin-right:5%;margin-bottom:5%;">
       <p class="storyTelling" style="margin-top:5%">
-				Hi, I'm Brian.  I’m a member of the public defined by Martin Gurri in <em>“The Revolt of The Public 
-				and the Crisis of Authority in the New Millennium,”</em> 2018.  You probably are too.  I was a reader early on, 
-				and that was about all it took.  A great story I was 
-				told in a red brick school house was bullshit.  Leaders lied.  Then it got worse.  Childhood fear 
-				of annihilation crumbled into kleptocracy.  Could a 28 year old single congressman 
-				being paid more than 3 2/3 times her district's average household income renounce her congressional 
-				health plan until all 325 million of us receive it?  If not you, then who, aoc?  535 of us clearly feel entitled to 
-				much of which few of their constituents can attain.  We selected an obvious liar 
-				as the face of our nation.  What is the sound of a national admission of 
-				powerlessness to govern?
+				Thank you for coming.  I’m Brian.  My goal here is <em>to end apportionment of political 
+				representation on the basis of anything other than headcount</em>.  Where is here?  Good 
+				question.  You’re probably reading a copy of this site from motia.com, but you can do 
+				all the same things, store keys, verify credentials, and see pages change dynamically 
+				reading the site from ipfs.io, or any other ipfs gateway server.  You think of reading 
+				your web pages from a location, but these pages can be served from any location running 
+				the Inter-Planetary File System, ipfs.
 			</p>
 			<p class="storyTelling">
-				But who needs kleptocrats?  Google owns our search history and splits our communication 
-				with Facebook.  Amazon owns our shopping habits.  Entertainment choices and movements are recorded by a 
-				handful of others.  Threat from artificial intelligence?  How closely held are the datasets capable to 
-				train deep learning systems?  Who needs kleptocrats when we give ourselves away like manhattan for a 
-				bag of beads, free email and social aprobation?
+				My political apportionment goal may take a while.  Between now and then I hope what 
+				I do here will be useful for people wanting to communicate authentically, know 
+				the provenance of news, automate organization, or reign sovereign over their 
+				data.  If we’re going to use ipfs for useful things, we’d at least better be able to 
+				encrypt our stuff since anyone can see it, from data in storage to personal messages 
+				left for our friends.  (You can always run a private network, 
+				but that’s not the ethos here.)  This site’s <em>demo</em> uses Stellar blockchain 
+				account keys like those used in crypto wallets to do its document encryption and 
+				signing.  Stellar accounts are also used for information storage, information pointer 
+				storage, and dynamic variable storage.
 			</p>
 			<p class="storyTelling">
-				While our financial and government leaders who lead us were leading us through 2008, foreclosing on the 
-				homes of millions while growing our money supply by 50% in five years to purchase 
-				wasting assets from the culpable, something new appeared.  Toy money?  Why not?  Easier 
-				to understand than a credit default swap.  It keeps accounts of itself.  Anyone can have a 
-				copy.  All of the copies agree.  Trust exists without investing it in people.  What if that trust can 
-				extend beyond peer to peer cash?  What if that trust can be invested in any of the documents we share  
-				between us?  I can do something constructive about accounting and documents.  Sexy, huh?  Read on.
+				Mashing the blockchain into document storage extends coverage of trust.  Specific ways 
+				that is done and also ways that is not done are mentioned later.  What is covered by 
+				blockchain trust, or consensus, will change, and methods of arriving at consensus will 
+				change.  The important concept is how consensus systems can extended to cover 
+				more than ledger pages of crypto accounts.  Stellar already does a lot, as you will see.  My demo 
+				has no size limits on trust coverage.  
 			</p>
 			<p class="storyTelling">
-				Gurri describes our revolt as a natural response to explosion of information.  If he is right, then we should 
-				expect our revolt to continue inexorably.  Gurri highlights dangers of nihilism, and we all see its 
-				predations.  The story I was told in the red brick schoolhouse may have been bullshit, but it was a good 
-				story.  For want of a better idea we could simply tell that story, tell it truthfully, and make sure it applies 
-				to everyone.  I present some primitive tools that can be used to construct transparent document systems, 
-				communicate authentically, and exchage value, in a space where we each may 
-				remain natural sovereigns of all pertaining to ourselves.  Any more may depend on you.
+				Abstracted trust appeared coincident with the 
+				banking crisis in 2008.  Millions upon millions had their homes foreclosed while the US 
+				diluted its money by 50% over five years, using the new cash to buy toxic assets for taxpayers 
+				from culpable bankers.  What timing!  Easier to understand than a credit default swap.  Anyone 
+				can copy the ledger.  All copies agree.  No investment of trust in humans.  What?  We won't 
+				need to invest trust in bankers and the federal government?  Oh forget it then; back to 
+				Facebook.
 			</p>
     </div>
 	`)], "mitao")]);
